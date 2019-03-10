f0c4bfc8bbafc012c3d31afd033231f4cb749ba5
Sean Lee <seanleecoder@gmail.com>
2019-03-11 00:07:24 +0800

    improving charts: onClick event

--- src/components/ChartsTab.js
+++ src/components/ChartsTab.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { VictoryPie } from "victory";
 
@@ -8,82 +8,76 @@ const mapStateToProps = state => {
   };
 };
 
-function ChartsTab(props) {
-  const { history } = props;
-  const durationHistory = history.length
-    ? history.map(function(item, index) {
-        const nextDatetime =
-          index !== history.length - 1
-            ? history[index + 1].datetime
-            : new Date();
-        return {
-          activity: item.activity,
-          detail: item.detail,
-          duration: Math.round((nextDatetime - item.datetime) / 1000 / 60)
-        };
-      })
-    : null;
-  console.log(JSON.stringify(durationHistory));
-  let data = groupBy(durationHistory, "activity");
-  let data2 = groupBy(
-    durationHistory.filter(item => (item.activity === "Work")),
-    "detail"
-  );
-  console.log(JSON.stringify(data2));
-  return (
-    <div>
-      <VictoryPie
-        data={data}
-        height={300}
-        padding={{ left: 0, top: 10, right: 0, bottom: 0 }}
-        colorScale={"qualitative"}
-        labelRadius={45}
-        labels={d =>
-          `${d.x} ${Math.floor(d.y / 60)}:${("0" + (d.y % 60)).slice(-2)}`
-        }
-        style={{ labels: { fill: "white", fontSize: 16 } }}
-        events={[
-          {
-            target: "data",
-            eventHandlers: {
-              onClick: () => {
-                return [
-                  {
-                    target: "data",
-                    mutation: props => {
-                      const fill = props.style && props.style.fill;
-                      return fill === "#c43a31"
-                        ? null
-                        : { style: { fill: "#c43a31" } };
+class ChartsTab extends Component {
+  state = {
+    selectedActivity: this.props.history[0].activity
+  };
+  render() {
+    const { history } = this.props;
+    const durationHistory = history.length
+      ? history.map(function(item, index) {
+          const nextDatetime =
+            index !== history.length - 1
+              ? history[index + 1].datetime
+              : new Date();
+          return {
+            activity: item.activity,
+            detail: item.detail,
+            duration: Math.round((nextDatetime - item.datetime) / 1000 / 60)
+          };
+        })
+      : null;
+    let data = groupBy(durationHistory, "activity");
+    let data2 = groupBy(
+      durationHistory.filter(
+        item => item.activity === this.state.selectedActivity
+      ),
+      "detail"
+    );
+    return (
+      <div>
+        <VictoryPie
+          data={data}
+          height={300}
+          padding={{ left: 0, top: 10, right: 0, bottom: 0 }}
+          colorScale={"qualitative"}
+          labelRadius={45}
+          labels={d =>
+            `${d.x} ${Math.floor(d.y / 60)}:${("0" + (d.y % 60)).slice(-2)}`
+          }
+          style={{ labels: { fill: "white", fontSize: 16 } }}
+          events={[
+            {
+              target: "data",
+              eventHandlers: {
+                onClick: () => {
+                  return [
+                    {
+                      target: "data",
+                      mutation: props => {
+                        this.setState({ selectedActivity: props.datum.x });
+                      }
                     }
-                  },
-                  {
-                    target: "labels",
-                    mutation: props => {
-                      return props.text === "clicked"
-                        ? null
-                        : { text: "clicked" };
-                    }
-                  }
-                ];
+                  ];
+                }
               }
             }
+          ]}
+        />
+        <VictoryPie
+          data={data2}
+          height={300}
+          padding={{ left: 0, top: 10, right: 0, bottom: 0 }}
+          colorScale={"heatmap"}
+          labelRadius={45}
+          labels={d =>
+            `${d.x} ${Math.floor(d.y / 60)}:${("0" + (d.y % 60)).slice(-2)}`
           }
-        ]}
-      />
-      <VictoryPie
-        data={data2}
-        height={300}
-        padding={{ left: 0, top: 10, right: 0, bottom: 0 }}
-        colorScale={"heatmap"}
-        labelRadius={45}
-        labels={d =>
-          `${d.x} ${Math.floor(d.y / 60)}:${("0" + (d.y % 60)).slice(-2)}`
-        }
-        style={{ labels: { fill: "white", fontSize: 16 } }}
-      />
-    </div>
-  );
+          style={{ labels: { fill: "white", fontSize: 16 } }}
+        />
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(ChartsTab);
