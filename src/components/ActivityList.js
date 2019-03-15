636434b7a340d70bc4014863849ea778638ea3eb
Sean Lee <seanleecoder@gmail.com>
2019-03-16 01:55:33 +0800

    testing react-beautiful-dnd: persist reordering

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -23,8 +23,38 @@ class ActivityList extends React.Component {
   state = initialData;
 
   onDragEnd = result => {
-    // TODO: reorder column
-    console.log("");
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    this.setState(newState);
   };
 
   render() {
