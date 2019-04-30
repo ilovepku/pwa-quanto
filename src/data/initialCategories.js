a79a1df34da911f38ac5c4fd46db9c307885acc7
Sean Lee <seanleecoder@gmail.com>
2019-04-30 23:06:54 +0800

    * generic initialCategories

--- src/data/initialCategories.js
+++ src/data/initialCategories.js
@@ -1,124 +1,81 @@
 const initialData = {
   details: {
-    "detail-1": { id: "detail-1", name: "Meal" },
-    "detail-2": { id: "detail-2", name: "Prepare" },
-    "detail-3": { id: "detail-3", name: "Snack" },
-    "detail-4": { id: "detail-4", name: "Aerobic" },
-    "detail-5": { id: "detail-5", name: "Strength" },
-    "detail-6": { id: "detail-6", name: "Game" },
-    "detail-7": { id: "detail-7", name: "Movie" },
-    "detail-8": { id: "detail-8", name: "Music" },
-    "detail-9": { id: "detail-9", name: "Read" },
-    "detail-10": { id: "detail-10", name: "Surfing" },
-    "detail-11": { id: "detail-11", name: "Tourism" },
-    "detail-12": { id: "detail-12", name: "TV" },
-    "detail-13": { id: "detail-13", name: "Beauty" },
-    "detail-14": { id: "detail-14", name: "Groceries" },
-    "detail-15": { id: "detail-15", name: "Health" },
-    "detail-16": { id: "detail-16", name: "Housework" },
-    "detail-17": { id: "detail-17", name: "Hygiene" },
-    "detail-18": { id: "detail-18", name: "Get Ready" },
-    "detail-19": { id: "detail-19", name: "Potty" },
-    "detail-20": { id: "detail-20", name: "Shop" },
-    "detail-21": { id: "detail-21", name: "Friend" },
-    "detail-22": { id: "detail-22", name: "Partner" },
-    "detail-23": { id: "detail-23", name: "Relative" },
-    "detail-24": { id: "detail-24", name: "Coding" },
-    "detail-25": { id: "detail-25", name: "Instrument" },
-    "detail-26": { id: "detail-26", name: "Language" },
-    "detail-27": { id: "detail-27", name: "Nap" },
-    "detail-28": { id: "detail-28", name: "Night" },
-    "detail-29": { id: "detail-29", name: "Drive" },
-    "detail-30": { id: "detail-30", name: "Intercity" },
-    "detail-31": { id: "detail-31", name: "Public" },
-    "detail-32": { id: "detail-32", name: "Taxi" },
-    "detail-33": { id: "detail-33", name: "Walk" },
-    "detail-34": { id: "detail-34", name: "Coding" },
-    "detail-35": { id: "detail-35", name: "Contact" },
-    "detail-36": { id: "detail-36", name: "Discuss" },
-    "detail-37": { id: "detail-37", name: "Job hunting" },
-    "detail-38": { id: "detail-38", name: "Meetings" },
-    "detail-39": { id: "detail-39", name: "Planning" },
-    "detail-40": { id: "detail-40", name: "Research" },
-    "detail-41": { id: "detail-41", name: "Testing" }
+    "detail-1": { id: "detail-1", name: "Reading" },
+    "detail-2": { id: "detail-2", name: "Meetings" },
+    "detail-3": { id: "detail-3", name: "Email" },
+    "detail-4": { id: "detail-4", name: "Phone Calls" },
+    "detail-5": { id: "detail-5", name: "Research" },
+    "detail-6": { id: "detail-6", name: "Discussion" },
+    "detail-7": { id: "detail-7", name: "Travel" },
+    "detail-8": { id: "detail-8", name: "Night" },
+    "detail-9": { id: "detail-9", name: "Nap" },
+    "detail-10": { id: "detail-10", name: "Breakfast" },
+    "detail-11": { id: "detail-11", name: "Brunch" },
+    "detail-12": { id: "detail-12", name: "Lunch" },
+    "detail-13": { id: "detail-13", name: "Dinner" },
+    "detail-14": { id: "detail-14", name: "Snack" },
+    "detail-15": { id: "detail-15", name: "Sports" },
+    "detail-16": { id: "detail-16", name: "Aerobic" },
+    "detail-17": { id: "detail-17", name: "Strength" },
+    "detail-18": { id: "detail-18", name: "Books" },
+    "detail-19": { id: "detail-19", name: "Newspapers" },
+    "detail-20": { id: "detail-20", name: "Magazines" },
+    "detail-21": { id: "detail-21", name: "Internet" }
   },
   activities: {
     "activity-1": {
       id: "activity-1",
-      name: "Eat",
-      detailIds: ["detail-1", "detail-2", "detail-3"]
+      name: "Work",
+      detailIds: [
+        "detail-1",
+        "detail-2",
+        "detail-3",
+        "detail-4",
+        "detail-5",
+        "detail-6",
+        "detail-7"
+      ]
     },
     "activity-2": {
       id: "activity-2",
-      name: "Exercise",
-      detailIds: ["detail-4", "detail-5"]
+      name: "Sleep",
+      detailIds: ["detail-8", "detail-9"]
     },
     "activity-3": {
       id: "activity-3",
-      name: "Fun",
+      name: "Eat",
       detailIds: [
-        "detail-6",
-        "detail-7",
-        "detail-8",
-        "detail-9",
         "detail-10",
         "detail-11",
-        "detail-12"
+        "detail-12",
+        "detail-13",
+        "detail-14"
       ]
     },
     "activity-4": {
       id: "activity-4",
-      name: "Life",
-      detailIds: [
-        "detail-13",
-        "detail-14",
-        "detail-15",
-        "detail-16",
-        "detail-17",
-        "detail-18",
-        "detail-19",
-        "detail-20"
-      ]
+      name: "Commute",
+      detailIds: []
     },
     "activity-5": {
       id: "activity-5",
-      name: "Social",
-      detailIds: ["detail-21", "detail-22", "detail-23"]
+      name: "Houework",
+      detailIds: []
     },
     "activity-6": {
       id: "activity-6",
-      name: "Study",
-      detailIds: ["detail-24", "detail-25", "detail-26"]
+      name: "Exercise",
+      detailIds: ["detail-15", "detail-16", "detail-17"]
     },
     "activity-7": {
       id: "activity-7",
-      name: "Sleep",
-      detailIds: ["detail-27", "detail-28"]
+      name: "Read",
+      detailIds: ["detail-18", "detail-19", "detail-20", "detail-21"]
     },
     "activity-8": {
       id: "activity-8",
-      name: "Transport",
-      detailIds: [
-        "detail-29",
-        "detail-30",
-        "detail-31",
-        "detail-32",
-        "detail-33"
-      ]
-    },
-    "activity-9": {
-      id: "activity-9",
-      name: "Work",
-      detailIds: [
-        "detail-34",
-        "detail-35",
-        "detail-36",
-        "detail-37",
-        "detail-38",
-        "detail-39",
-        "detail-40",
-        "detail-41"
-      ]
+      name: "TV",
+      detailIds: []
     }
   },
   activityIds: [
@@ -129,8 +86,7 @@ const initialData = {
     "activity-5",
     "activity-6",
     "activity-7",
-    "activity-8",
-    "activity-9"
+    "activity-8"
   ]
 };
 
