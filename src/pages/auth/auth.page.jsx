a1d1fb9ac1a4b2bc4986290564b68c3c6d39e030
Sean Lee <seanleecoder@gmail.com>
2019-11-20 15:27:58 +0800

    * disable unused 3rd party login providers

--- src/pages/auth/auth.page.jsx
+++ src/pages/auth/auth.page.jsx
@@ -28,10 +28,10 @@ const firebaseUiConfig = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID/* ,
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID */
   ],
   credentialHelper: "none"
 };
